a7741c32279d782e4a6acdb59b514325e7963ee2
AsusCerry <cerry2024@foxmail.com>
2025-03-27 00:38:23 +0800

    Update

--- vitepress-starter/deploy.sh
+++ vitepress-starter/deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # Jacqueline712/mondaylab-blog 替换为自己的用户名和对应的仓库名
 # 意思为将master构建后的代码合并到gh-pages分支上，然后在gh-pages分支上部署~
 
-git push -f git@github.com:Cerry2022/Cerry2022.github.io.git master:gh-pages
+git push -f origin master:gh-pages
 
 cd -
